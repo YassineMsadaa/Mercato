cb9ca72d0c675322d65c4c5c73a9741931ae2802
Fares Ben Ammar <49375342+FaresBenAmmar23@users.noreply.github.com>
2021-04-22 01:56:47 +0200

    Add files via upload

---
+++ Avis.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Avis
+ *
+ * @ORM\Table(name="avis", indexes={@ORM\Index(name="user_id", columns={"user_id"})})
+ * @ORM\Entity
+ */
+class Avis
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="avis", type="string", length=200, nullable=true)
+     * @Assert\NotBlank(message="vous devez remplir la case avis")
+     * @Assert\Length( min = 5, max = 200)
+     */
+    private $avis;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="date", type="date", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="type", type="string", length=20, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="user_id", type="integer", nullable=true)
+     */
+    private $user;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAvis(): ?string
+    {
+        return $this->avis;
+    }
+
+    /**
+     * @param string|null $avis
+     */
+    public function setAvis(?string $avis): void
+    {
+        $this->avis = $avis;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param \DateTime|null $date
+     */
+    public function setDate(?\DateTime $date): void
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string|null $type
+     */
+    public function setType(?string $type): void
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getUser(): ?int
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param int|null $user
+     */
+    public function setUser(?int $user): void
+    {
+        $this->user = $user;
+    }
+
+
+}
---
+++ ReclamationController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reclamation;
+use App\Form\ReclamationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/reclamation")
+ */
+class ReclamationController extends AbstractController
+{
+    /**
+     * @Route("/front", name="reclamation_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $reclamations = $this->getDoctrine()
+            ->getRepository(Reclamation::class)
+            ->findAll();
+
+        return $this->render('reclamation/index.html.twig', [
+            'reclamations' => $reclamations,
+        ]);
+    }
+
+    /**
+     * @Route("/back", name="reclamation_indexb", methods={"GET"})
+     */
+    public function indexb(): Response
+    {
+        $reclamations = $this->getDoctrine()
+            ->getRepository(Reclamation::class)
+            ->findAll();
+
+        return $this->render('reclamation_back/index.html.twig', [
+            'reclamations' => $reclamations,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="reclamation_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $reclamation = new Reclamation();
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $reclamation->setDate(new \DateTime('now'));
+
+            $reclamation->setUserId(2);
+            $entityManager->persist($reclamation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('reclamation_index');
+        }
+
+        return $this->render('reclamation/new.html.twig', [
+            'reclamation' => $reclamation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="reclamation_show", methods={"GET"})
+     */
+    public function show(Reclamation $reclamation): Response
+    {
+        return $this->render('reclamation/show.html.twig', [
+            'reclamation' => $reclamation,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="reclamation_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Reclamation $reclamation): Response
+    {
+        $form = $this->createForm(ReclamationType::class, $reclamation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reclamation->setDate(new \DateTime('now'));
+
+            $reclamation->setUserId(2);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('reclamation_index');
+        }
+
+        return $this->render('reclamation/edit.html.twig', [
+            'reclamation' => $reclamation,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="reclamation_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Reclamation $reclamation): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$reclamation->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($reclamation);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('reclamation_index');
+    }
+}
