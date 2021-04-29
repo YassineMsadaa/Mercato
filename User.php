a2c8c267197093f347213458121cf52a1c4400cd
nourzidi <47746624+nourzidi@users.noreply.github.com>
2021-04-29 11:24:01 +0200

    Add files via upload

--- User.php
+++ User.php
@@ -2,523 +2,172 @@
 
 namespace App\Entity;
 
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 /**
- * User
- *
- * @ORM\Table(name="user", uniqueConstraints={@ORM\UniqueConstraint(name="UNIQ_8D93D649F85E0677", columns={"username"})})
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"emailadresse"},
+ * message="Already used")
  */
 class User
 {
     /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
      */
     private $id;
 
+
+
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="username", type="string", length=180, nullable=true)
+     * @ORM\Column(type="string", length=30, nullable=true)
      */
     private $username;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="roles", type="string", length=10, nullable=true)
-     */
-    private $roles;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="password", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=30, nullable=true)
+
+     * @Assert\Length(min="8",minMessage="your password must be at least 8 characters long")
+     * @Assert\EqualTo(propertyPath="confirm_password",message="
+    you did not type the same password")
      */
+
     private $password;
-
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+
+     * @Assert\EqualTo(propertyPath="password",message="
+    you did not type the same password")
      */
-    private $email;
+    public $confirm_password;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=30, nullable=true)
      */
     private $name;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="last_name", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=80, nullable=true)
      */
-    private $lastName;
+   public $s_ques;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="birth_date", type="string", length=12, nullable=true)
+     * @ORM\Column(type="string", length=30, nullable=true)
      */
-    private $birthDate;
+    private $answer;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="speciality", type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=20, nullable=true)
      */
-    private $speciality;
+    private $type;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="status", type="string", length=255, nullable=true)
-     */
-    private $status;
+     * @ORM\Column(type="string", length=30, nullable=true)
+     * @Assert\Email (
+     *     message = "The emailadresse '{{ value }}' is not a valid emailadresse."
+     * )
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="experience", type="string", length=255, nullable=true)
      */
-    private $experience;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="hight", type="integer", nullable=true)
-     */
-    private $hight;
+    private $emailadresse;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="weight", type="integer", nullable=true)
-     */
-    private $weight;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="cv", type="string", length=255, nullable=true)
-     */
-    private $cv;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="media", type="string", length=255, nullable=true)
-     */
-    private $media;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="company", type="string", length=255, nullable=true)
-     */
-    private $company;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="position", type="string", length=255, nullable=true)
-     */
-    private $position;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="pays_natals", type="string", length=30, nullable=true)
-     */
-    private $paysNatals;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="sexe", type="string", length=30, nullable=true)
-     */
-    private $sexe;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="lien_profil_pic", type="string", length=100, nullable=true)
-     */
-    private $lienProfilPic;
-
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="sponsorship", type="string", length=30, nullable=true)
-     */
-    private $sponsorship;
-
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="solde", type="integer", nullable=true)
-     */
-    private $solde;
-
-    /**
-     * @return int
-     */
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
 
-    /**
-     * @return string|null
-     */
+
     public function getUsername(): ?string
     {
         return $this->username;
     }
 
-    /**
-     * @param string|null $username
-     */
-    public function setUsername(?string $username): void
+    public function setUsername(?string $username): self
     {
         $this->username = $username;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getRoles(): ?string
-    {
-        return $this->roles;
-    }
-
-    /**
-     * @param string|null $roles
-     */
-    public function setRoles(?string $roles): void
-    {
-        $this->roles = $roles;
-    }
-
-    /**
-     * @return string|null
-     */
     public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    /**
-     * @param string|null $password
-     */
-    public function setPassword(?string $password): void
+    public function setPassword(string $password): self
     {
         $this->password = $password;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param string|null $email
-     */
-    public function setEmail(?string $email): void
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @return string|null
-     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
-    /**
-     * @param string|null $name
-     */
-    public function setName(?string $name): void
+    public function setName(?string $name): self
     {
         $this->name = $name;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getLastName(): ?string
+    public function getSQues(): ?string
     {
-        return $this->lastName;
+        return $this->s_ques;
     }
 
-    /**
-     * @param string|null $lastName
-     */
-    public function setLastName(?string $lastName): void
+    public function setSQues(?string $s_ques): self
     {
-        $this->lastName = $lastName;
+        $this->s_ques = $s_ques;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getBirthDate(): ?string
+    public function getAnswer(): ?string
     {
-        return $this->birthDate;
+        return $this->answer;
     }
 
-    /**
-     * @param string|null $birthDate
-     */
-    public function setBirthDate(?string $birthDate): void
+    public function setAnswer(?string $answer): self
     {
-        $this->birthDate = $birthDate;
+        $this->answer = $answer;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getSpeciality(): ?string
+    public function getType(): ?string
     {
-        return $this->speciality;
+        return $this->type;
     }
 
-    /**
-     * @param string|null $speciality
-     */
-    public function setSpeciality(?string $speciality): void
+    public function setType(?string $type): self
     {
-        $this->speciality = $speciality;
+        $this->type = $type;
+
+        return $this;
     }
 
-    /**
-     * @return string|null
-     */
-    public function getStatus(): ?string
+    public function getEmailadresse(): ?string
     {
-        return $this->status;
+        return $this->emailadresse;
     }
 
-    /**
-     * @param string|null $status
-     */
-    public function setStatus(?string $status): void
+    public function setEmailadresse(?string $emailadresse): self
     {
-        $this->status = $status;
+        $this->emailadresse = $emailadresse;
+
+        return $this;
     }
-
-    /**
-     * @return string|null
-     */
-    public function getExperience(): ?string
-    {
-        return $this->experience;
-    }
-
-    /**
-     * @param string|null $experience
-     */
-    public function setExperience(?string $experience): void
-    {
-        $this->experience = $experience;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getHight(): ?int
-    {
-        return $this->hight;
-    }
-
-    /**
-     * @param int|null $hight
-     */
-    public function setHight(?int $hight): void
-    {
-        $this->hight = $hight;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getWeight(): ?int
-    {
-        return $this->weight;
-    }
-
-    /**
-     * @param int|null $weight
-     */
-    public function setWeight(?int $weight): void
-    {
-        $this->weight = $weight;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getCv(): ?string
-    {
-        return $this->cv;
-    }
-
-    /**
-     * @param string|null $cv
-     */
-    public function setCv(?string $cv): void
-    {
-        $this->cv = $cv;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getMedia(): ?string
-    {
-        return $this->media;
-    }
-
-    /**
-     * @param string|null $media
-     */
-    public function setMedia(?string $media): void
-    {
-        $this->media = $media;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getCompany(): ?string
-    {
-        return $this->company;
-    }
-
-    /**
-     * @param string|null $company
-     */
-    public function setCompany(?string $company): void
-    {
-        $this->company = $company;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getPosition(): ?string
-    {
-        return $this->position;
-    }
-
-    /**
-     * @param string|null $position
-     */
-    public function setPosition(?string $position): void
-    {
-        $this->position = $position;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getPaysNatals(): ?string
-    {
-        return $this->paysNatals;
-    }
-
-    /**
-     * @param string|null $paysNatals
-     */
-    public function setPaysNatals(?string $paysNatals): void
-    {
-        $this->paysNatals = $paysNatals;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSexe(): ?string
-    {
-        return $this->sexe;
-    }
-
-    /**
-     * @param string|null $sexe
-     */
-    public function setSexe(?string $sexe): void
-    {
-        $this->sexe = $sexe;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getLienProfilPic(): ?string
-    {
-        return $this->lienProfilPic;
-    }
-
-    /**
-     * @param string|null $lienProfilPic
-     */
-    public function setLienProfilPic(?string $lienProfilPic): void
-    {
-        $this->lienProfilPic = $lienProfilPic;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getSponsorship(): ?string
-    {
-        return $this->sponsorship;
-    }
-
-    /**
-     * @param string|null $sponsorship
-     */
-    public function setSponsorship(?string $sponsorship): void
-    {
-        $this->sponsorship = $sponsorship;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getSolde(): ?int
-    {
-        return $this->solde;
-    }
-
-    /**
-     * @param int|null $solde
-     */
-    public function setSolde(?int $solde): void
-    {
-        $this->solde = $solde;
-    }
-
-
 }
--- UserController.php
+++ UserController.php
@@ -2,77 +2,157 @@
 
 namespace App\Controller;
 
+
 use App\Entity\User;
+
 use App\Form\UserType;
+use App\Repository\UserRepository;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use phpDocumentor\Reflection\DocBlock\Serializer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @Route("/user")
- */
+use Symfony\Component\Routing\Loader\XmlFileLoader;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/", name="user_index", methods={"GET"})
-     */
-    public function index(): Response
-    {
-        $users = $this->getDoctrine()
-            ->getRepository(User::class)
-            ->findAll();
 
-        return $this->render('user/index.html.twig', [
-            'users' => $users,
+
+
+
+/* /**
+     * @Route("/lol1", name="lol1")
+
+    public function manager(): Response
+    {
+
+        $rep=$this->getDoctrine()->getRepository(User::class);
+        $user=$rep->findAll();
+
+
+
+        return $this->render('user/companymanager.html.twig', [
+            'user' => $user,
         ]);
-    }
+    }*/
+
 
     /**
-     * @Route("/new", name="user_new", methods={"GET","POST"})
+     * @Route("/userfront", name="userfront")
      */
-    public function new(Request $request): Response
+    public function new1(Request $request)
     {
-        $user = new User();
+        $user =new User();
         $form = $this->createForm(UserType::class, $user);
+        $form->add('save',SubmitType::class);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+
             $entityManager = $this->getDoctrine()->getManager();
+
             $entityManager->persist($user);
             $entityManager->flush();
+            //mailing
+            $mail = new PHPMailer(true);
 
-            return $this->redirectToRoute('user_index');
+            try {
+
+                $Username = $form->get('username')->getData();
+
+
+                $email = $form->get('emailadresse')->getData();
+
+
+                //Server settings
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+                $mail->isSMTP();
+                $mail->Host       = 'smtp.gmail.com';
+                $mail->SMTPAuth   = true;
+                $mail->Username   = 'souissieya2018@gmail.com';             // SMTP username
+                $mail->Password   = 'Youyou2020';                               // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port       = 587;
+
+                //Recipients
+                $mail->setFrom('nour.zidi@esprit.tn', 'mercato User');
+                $mail->addAddress($email, 'mercato user');     // Add a recipient
+
+                // Content
+                $corps="Bonjour Monsieur/Madame  ".$Username. "voici votre copie de vos informations  est comme suite"  ;
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = 'Sois le Bienvenue!';
+                $mail->Body    = $corps;
+
+                $mail->send();
+
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+
+            //end mailing
+            return $this->redirectToRoute('lol1');
+
         }
-
         return $this->render('user/new.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
         ]);
     }
 
+
+
     /**
-     * @Route("/{id}", name="user_show", methods={"GET"})
+     * @Route("/lol1", name="lol1")
      */
-    public function show(User $user): Response
+    public function manager(): Response
     {
-        return $this->render('user/show.html.twig', [
+
+        $rep=$this->getDoctrine()->getRepository(User::class);
+        $user=$rep->findAll();
+
+
+
+        return $this->render('user/companymanager.html.twig', [
             'user' => $user,
         ]);
     }
-
     /**
-     * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
+     * @Route("/company_index", name="company_index")
+
      */
-    public function edit(Request $request, User $user): Response
+    public function index3(UserRepository $userRepository): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $userRepository->findAll(),
+        ]);
+    }
+    /**
+     * @Route("/companyeditm{id}", name="companyeditm")
+     */
+    public function edit(Request $request, User $user)
     {
         $form = $this->createForm(UserType::class, $user);
+        $form->add('save',SubmitType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_index');
+            return $this->redirectToRoute('lol1');
         }
 
         return $this->render('user/edit.html.twig', [
@@ -80,9 +160,65 @@ class UserController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
-
     /**
-     * @Route("/{id}", name="user_delete", methods={"POST"})
+     * @Route("/listu1", name="listu1", methods={"GET"})
+     */
+    public function listu1(UserRepository $userRepository): Response
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('user\pdf.html.twig', [
+            'users' =>$userRepository->findAll(),
+        ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A2', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+        // Output the generated PDF to Browser (inline view)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+    /**
+     * @Route("/company_show{id}", name="company_show")
+     */
+    public function show(int $id): Response
+    {
+        $rep=$this->getDoctrine()->getRepository(User::class);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user=$rep->find($id);
+
+
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);}
+    /**
+     * @Route("/sortbytitleasc", name="sortbytitleasc")
+     */
+    public function sortByTitleASC(): Response
+    {
+
+        $rep=$this->getDoctrine()->getRepository(User::class);
+        $user=$rep->sortByTitleASC();
+
+
+        return $this->render('user/companymanager.html.twig', [
+            'user' => $user,
+        ]);
+    }
+    /**
+     * @Route("/company_detele{id}", name="company_delete")
      */
     public function delete(Request $request, User $user): Response
     {
@@ -92,6 +228,39 @@ class UserController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('user_index');
+        return $this->redirectToRoute('lol1');
     }
+    /**
+     * @Route("/usera", name="usera")
+     */
+    public function new2(Request $request)
+    {
+        $user =new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->add('save',SubmitType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('lol1');
+
+        }
+        return $this->render('user/ajoutmanager.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
+
+
+
 }
